d0e40e2bb23e462f81f0a1ddf206665874834e6c
dannydagamaisutic@gmail.com <dannydagamaisutic@gmail.com>
2023-05-25 11:59:54 +0100

    Showing how to acess a enum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import enumeration.Color;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Color c1= Color.BLUE;
+
+        System.out.println("Acessendo via Nome " +c1.name());
+        System.out.println("Acessando o Valor " +c1.getValor());
+        for (Color color : Color.values()){
+            System.out.println(color.getValor());
+            System.out.println(color.name());
+        }
+        //System.out.println(c1);
     }
 }
